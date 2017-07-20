a10ff349e35cd5ce5e40bac495b545dad26b7cce
An Ngo <a.ngo20@students.bchigh.edu>
2017-07-20 17:31:04 +0000

    todo # variables

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,7 +3,19 @@ class TodoController < ApplicationController
     end
 
     def show
-        @todo_description="sleep"
-        @todo_pomodoro_estimate="20"
+        todo_id=params[:id]
+        if todo_id =="1"
+            @todo_description = "eat"
+            @todo_pomodoro_estimate = "9"
+        elsif todo_id == "2"
+            @todo_description = "sleep"
+            @todo_pomodoro_estimate = 10
+            
+        elsif todo_id == "3"
+            @todo_description = "code"
+            @todo_pomodoro_estimate = 2
+            
+        end
     end
+    
 end
