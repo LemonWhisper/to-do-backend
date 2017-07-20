95f539633e51b639c32d7f7e5fa1fb0a1b2f3dc4
An Ngo <a.ngo20@students.bchigh.edu>
2017-07-20 19:05:28 +0000

    done with database

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,19 +3,7 @@ class TodoController < ApplicationController
     end
 
     def show
-        todo_id=params[:id]
-        if todo_id =="1"
-            @todo_description = "eat"
-            @todo_pomodoro_estimate = "9"
-        elsif todo_id == "2"
-            @todo_description = "sleep"
-            @todo_pomodoro_estimate = 10
-            
-        elsif todo_id == "3"
-            @todo_description = "code"
-            @todo_pomodoro_estimate = 2
-            
-        end
+        @todo=Todo.find_by_id(params[:id])
     end
     
 end
