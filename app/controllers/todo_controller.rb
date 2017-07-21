d832ea13f95a76953669a4aa1303add835aabd9b
An Ngo <a.ngo20@students.bchigh.edu>
2017-07-21 14:17:18 +0000

    Finished Spartan Camp

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,5 +1,6 @@
 class TodoController < ApplicationController
     def index
+        @todos = Todo.all
     end
 
     def show
@@ -7,6 +8,14 @@ class TodoController < ApplicationController
     end
     def edit
         @todo=Todo.find_by_id(params[:id])
+    end
+    
+    def create
+        t = Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro-estimate']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
     end
     
     def update
@@ -17,7 +26,10 @@ class TodoController < ApplicationController
         redirect_to "/todo/show/#{t.id}"
     end
     
-
-    def delete
+    def destroy
+        t = Todo.find_by_id(params[:id])
+        t.destroy
+        redirect_to "/"
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-root to: 'todo#index'
-get'todo/show/:id', to:'todo#show'
-get 'todo/edit/:id', to:'todo#edit'
-get 'todo/update/:id', to: 'todo#update'
-
-
+    root to: 'todo#index'
+    get'todo/show/:id', to:'todo#show'
+    get 'todo/edit/:id', to:'todo#edit'
+    get 'todo/update/:id', to: 'todo#update'
+    get 'todo/new', to: 'todo#new'
+    get 'todo/create', to: 'todo#create'
+    get 'todo/destroy/:id', to: 'todo#destroy'
 
 end
